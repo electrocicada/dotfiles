2c3045a6fc5640369e67ebe1ca8020d319174abb
electrocicada <stasderevyanko03@gmail.com>
2022-02-26 12:28:09 +0200

    add fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,21 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+set fish_greeting
+
+alias config='/usr/bin/git --git-dir=/home/electrocicada/.cfg/ --work-tree=/home/electrocicada'
+
+#vim))
+alias vmi='vim'
+alias imv='vim'
+alias ivm='vim'
+alias mvi='vim'
+alias miv='vim'
+
+#clear
+alias c='clear'
+
+#replase ls via exa
+alias ll='exa -lahH'
+alias ls='exa -F'
---
+++ .config/fish/functions/fish_prompt.fish
@@ -0,0 +1,1 @@
+/home/electrocicada/.local/share/omf/themes/boxfish/fish_prompt.fish
